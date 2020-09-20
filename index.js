b4d911115677a737163460e1e399b8242630379f
jman11111 <jbroncato@yahoo.com>
2020-09-20 12:46:21 -0500

    routing,config,and debug

--- index.js
+++ index.js
@@ -1,77 +1,21 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi');
+const blasters = require('./routes/blasters')
+const config = require('config');
+const startupDebugger = require('debug')('app:startup');
+const databaseDebugger = require('debug')('app:db');
 // express, joi
+//USE DEBUG instead of console.log
+//config can be used for env variables such as passwords, or ones that change based on development phase
+//Make file for each resource in Routes folder when it comes to file structure
+
+
+startupDebugger('Starting up');
+
+console.log(config.get('name'));
 
 app.use(express.json());
-
-var blasters = [
-    {id: 1,name: "stryfe"},
-    {id: 15,name: "hammershot"}
-]
-
-app.get('/', (req,res) => {
-    res.send('hello person');
-});
-
-app.get('/api/blasters', (req,res) => {
-    res.send(blasters);
-});
-
-app.post('/api/blasters',(req,res) => {
-
-    const { error } = validateBlaster(req.body);
-    // either has null error field if successful, or vice versa
-
-    if (error) return res.status(400).send(error);
-    
-    const blaster = {
-        id: blasters.length + 1,
-        name: req.body.name
-    }
-    blasters.push(blaster);
-    res.send(blaster);
-});
-
-app.put('/api/blasters/:id',(req,res) => {
-    let blaster = blasters.find((element) => element.id === parseInt(req.params.id));
-    if (!blaster) return res.status(404).send("Could not find blaster with that ID")
-
-    const { error } = validateBlaster(req.body);
-    // either has null error field if successful, or vice versa
-
-    if (error) return res.status(400).send(error);
-
-    blaster.name = req.body.name;
-    res.send(blaster);
-    
-});
-
-function validateBlaster(blaster){
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    return schema.validate(blaster)
-    // either has null error field if successful, or vice versa
-
-}
-
-app.get('/api/blasters/:id', (req,res) => {
-    let blaster = blasters.find((element) => element.id === parseInt(req.params.id));
-    if(!blaster) return res.status(400).send("Blaster not found");
-    res.send(blaster);
-});
-
-app.delete('/api/blasters/:id', (req,res) => {
-    let blaster = blasters.find((element) => element.id === parseInt(req.params.id));
-    if(!blaster) return res.status(400).send("Blaster not found");
-    const index = blasters.indexOf(blaster);
-    blasters.splice(index,1);
-
-    res.send(blaster);
-
-});
+app.use('/api/blasters',blasters);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
